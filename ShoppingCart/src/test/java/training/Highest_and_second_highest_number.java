1d64805f60711276b55deded536f25d09cd67402
VenkateshRavi27 <venkatesh.study96@gmail.com>
2025-06-17 19:10:36 +0530

    code updated

--- ShoppingCart/src/test/java/training/Highest_and_second_highest_number.java
+++ ShoppingCart/src/test/java/training/Highest_and_second_highest_number.java
@@ -1,25 +1,22 @@
 package training;
 
+import java.util.Arrays;
+
 public class Highest_and_second_highest_number {
 
 	public static void main(String[] args) {
 
 		int[] num = {10, 45, 22, 89, 86, 33, 32};
 
-		int firstMax = Integer.MIN_VALUE;
-		int secondMax = Integer.MIN_VALUE;
-
-		for (int n : num) {
-		    if (n > firstMax) {
-		        secondMax = firstMax;
-		        firstMax = n;
-		    } else if (n > secondMax && n < firstMax) {
-		        secondMax = n;
-		    }
-		}
-
-		System.out.println("Highest number: " + firstMax);
-		System.out.println("Second highest number: " + secondMax);
+		Arrays.sort(num);
+		
+		int firstMax = num[num.length -1];
+		
+		int secondMax = num[num.length -2];
+		
+		System.out.println("First Max No : " +firstMax);
+		
+		System.out.println("Second Max No : " +secondMax);
 
 	}
 
