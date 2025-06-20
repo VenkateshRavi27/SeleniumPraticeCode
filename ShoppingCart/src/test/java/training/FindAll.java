21d9670dc8ff56ca93fcf17302afdac41f119b5c
VenkateshRavi27 <venkatesh.study96@gmail.com>
2025-06-20 11:29:31 +0530

    new code added

---
+++ ShoppingCart/src/test/java/training/FindAll.java
@@ -0,0 +1,50 @@
+package training;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import driver.DriverInstance;
+
+public class FindAll extends DriverInstance {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.amazon.in/");
+
+		wait = new WebDriverWait(driver, 10);
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		//// span[contains(text(), 'Apple iPhone')]
+
+		WebElement searchProudct = driver.findElementByXPath("//input[@placeholder='Search Amazon.in']");
+		
+		wait.until(ExpectedConditions.visibilityOf(searchProudct)).sendKeys("iphone" , Keys.ENTER);
+		
+		List<WebElement> iphoneName = driver.findElementsByTagName("h2");
+		
+		for (WebElement result : iphoneName) {
+			
+			System.out.println(result.getText());
+			
+		}
+		
+		driver.quit();
+		
+		
+
+	}
+
+}
